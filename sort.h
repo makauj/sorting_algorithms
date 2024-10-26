82c8bc7056fa3c444504ec6c59a1aeb0f72e3a91
makau <makauwanyoike@gmail.com>
2024-10-26 14:47:32 +0300

    sort header

--- sort.h
+++ sort.h
@@ -18,4 +18,10 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/* Bubble sort prototypes */
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
 #endif /* SORT_H */
