4ae11213cbb7703a8e5e2025e24e27b90b1b6b61
makau <makauwanyoike@gmail.com>
2024-10-29 14:39:06 +0300

    correction

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -9,25 +9,25 @@
 
 void shell_sort(int *array, size_t size)
 {
-	int interval, temp;
-	size_t j, i;
+	int temp;
+	size_t j, i, interval;
 
 	if (!array || size < 2)
 		return;
 
 	for (interval = size / 2; interval > 0; interval /= 2)
 	{
-		for (i = interval; i < size; i += 1)
+		for (i = interval; i < size; i++)
 		{
 			temp = array[i];
-			for (j = i; (int)j >= interval && array[j - interval] > temp; j -= interval)
+			for (j = i; j >= interval && array[j - interval] > temp; j -= interval)
 			{
 				array[j] = array[j - interval];
 			}
 			array[j] = temp;
 
-			print_array(array, size);
 		}
+		print_array(array, size);
 	}
 }
 
